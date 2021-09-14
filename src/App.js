eaf440d6e5ab1ad312fdc04323b7b6b8c1b1480b
mksya <mik.sam@hotmail.fr>
2021-09-14 17:01:50 +0200

    Part1 & Part2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Counter from "./components/counter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-3">
+       <Counter title="Profile " value={1} image="images/profile.jpg" />
+       <Counter title="Dolphin " value={1} image="images/profile.jpg" />
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/counter.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Counter extends React.Component {
+
+    constructor(props){
+       super(props);
+       this.state={
+        counter:1,
+        list:[0]
+       }
+    }
+
+    compute=(op)=>{
+        let sign=op==='+'?1:-1;
+        if(this.state.counter==1 && op==='-') sign=0;
+        let c=this.state.counter+sign;
+
+        this.setState({
+            counter:c,
+            list:new Array(c).fill(0)
+            });
+    }
+
+    render(){
+        return(
+        <div className="card">
+            <div className="card-header">
+                <strong>
+                    {this.props.title} : {this.state.counter}
+                </strong>
+              </div>
+              <div className="m1-auto">
+                 <button onClick={()=>this.compute('+')} className="btn btn-primary m-2">+</button>
+                 <button onClick={()=>this.compute('-')} className="btn btn-primary m-2">-</button>
+              </div>
+            <div className="card-body">
+                {
+                    this.state.list.map((T,number)=>
+                        <span>{number}
+                        <img width={100} src={this.props.image}/>
+                        </span>
+                    )
+                }
+            </div>
+        </div>
+        );
+
+    };
+
+}
+
+export default Counter;
